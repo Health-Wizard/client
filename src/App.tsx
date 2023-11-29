5658c6ab21a9899e47a17bd37b00671e6bfdc8be
techyNonso <williamikeji@gmail.com>
2023-11-29 09:51:39 +0100

    employee listing done

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,20 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Dashboard } from "@modules/Dashboard";
 import { Onboarding } from "@modules/Onboarding";
+import { Employees } from "@modules/Employees";
 
 const queryClient = new QueryClient();
 
 function App() {
-  
   return (
     <QueryClientProvider client={queryClient}>
       <div className=" h-screen bg-bodyBg   ">
         <Toaster position="top-right" reverseOrder={false} />
-        
+
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/employees" element={<Employees />} />
         </Routes>
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
---
+++ src/modules/Sample Structure/Dashboard.tsx
@@ -0,0 +1,9 @@
+import Layout from "@modules/Shared/layout/Layout";
+
+export const Dashboard = () => {
+  return (
+    <Layout>
+      <div className="h-full w-full ">Dashboard</div>
+    </Layout>
+  );
+};
