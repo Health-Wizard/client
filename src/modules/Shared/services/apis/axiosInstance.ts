0ac0725c34407721b6f6242f0110ea630b0d70ee
sayantan1413 <sayantanbose2001@gmail.com>
2023-12-02 12:11:52 +0530

    Added production url

--- src/modules/Shared/services/apis/axiosInstance.ts
+++ src/modules/Shared/services/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const AxiosClient = axios.create({
-	baseURL: 'http://localhost:8080/api/v1',
+	baseURL: 'https://employee-health-backend-6h0l.onrender.com/api/v1',
 	timeout: 8000,
 	headers: {
 		Accept: 'application/json',
